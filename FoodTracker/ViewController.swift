62c6181c4e9a1c6b726ec7b3b9257388b1a8cf0d
Jörg Klausewitz <bytepunch@gmail.com>
2019-08-12 12:39:06 +0200

    Custom star rating control implemented

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     // MARK: Properties
-    
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
-    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     
     override func viewDidLoad() {
@@ -24,11 +24,19 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
 
     // MARK: Actions
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
         
-        mealNameLabel.text = "Default text"
+        // Hide the keyboard.
+        nameTextField.resignFirstResponder()
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+        
         
     }
+    
     
     // MARK: UITextFieldDelegate
     
@@ -44,6 +52,24 @@ class ViewController: UIViewController, UITextFieldDelegate {
         mealNameLabel.text = textField.text
         
     }
+    
+    //MARK: UIImagePickerControllerDelegate
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        photoImageView.image = selectedImage
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
     
 }
 
